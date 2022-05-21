abe784ff75a9d22b76b7f2a4e0ef35a9ebb35984
Daryl Rogony <darylrogony@gmail.com>
2022-05-21 12:04:33 +0300

    Completed routing lab

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,23 @@
 import React from "react";
 import { actors } from "../data";
 
-function Actors() {
-  return <div>{/*{code here}*/}</div>;
+function Actors () {
+   const actorsInfo = actors.map((actor) => (
+    <div key={actor.name}>
+      <h1>{actor.name}</h1>
+      <ul>
+        {actor.movies.map((movie) =>(
+          <li key={movie}>{movie}</li>
+     ))}
+      </ul>
+    </div>
+  ));
+  return (
+    <div>
+      <h1> Actors Page </h1>
+      {actorsInfo}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,14 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+     <div className="navbar">
+       <NavLink exact to="/">Home</NavLink>
+       <NavLink exact to="/Movies">Movies</NavLink>
+       <NavLink exact to="/Directors">Directors</NavLink>
+       <NavLink exact to="/Actors">Actors</NavLink>
+     </div> 
+ );
 }
 
 export default NavBar;
